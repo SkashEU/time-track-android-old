4d076bb9fd87a4a48c8e3e79224f589ff1898c2c
0xSkash <48493983+0xSkash@users.noreply.github.com>
2023-03-14 16:58:12 +0100

    created base login view, added theme and colors

---
+++ app/src/main/java/com/skash/timetrack/TimeTrack.kt
@@ -0,0 +1,11 @@
+package com.skash.timetrack
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class TimeTrack: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/skash/timetrack/auth/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.skash.timetrack.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import com.skash.timetrack.R
+
+class LoginActivity: AppCompatActivity(R.layout.activity_login) {
+}
